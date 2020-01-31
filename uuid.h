de4d6204d0e13934cec3d2e911014d4052bc4a9a
Staffan Thomen <duck@shangtai.net>
2020-01-31 18:38:41 +0200

    Add a macro for uuid_generate that runs uuid_generate_random

--- uuid.h
+++ uuid.h
@@ -3,6 +3,8 @@
 
 typedef unsigned char uuid_t[16];
 
+#define uuid_generate(out) uuid_generate_random(out)
+
 extern void uuid_generate_random(uuid_t out);
 extern void uuid_unparse(const uuid_t uuid, char *out);
 extern void uuid_copy(uuid_t dst, const uuid_t src);
